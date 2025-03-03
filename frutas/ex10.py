d408df307ecea8fe91b7530359b8e2d1ed342b7f
Christian Machado <realchris.machado@gmail.com>
2025-03-03 18:23:35 -0300

    Calculando dinamicamente os dados da coluna 'Total' de cada aba da planilha

--- frutas/ex10.py
+++ frutas/ex10.py
@@ -9,15 +9,17 @@ wb = load_workbook('dados.xlsx')
 sheets = ['Janeiro', 'Fevereiro', 'Março', 'Resumo']
 headers = ['Produto', 'Quantidade', 'Preço Unitário', 'Total']
 
-for sheet in sheets:
-    ws = wb.create_sheet(f'{sheet}')
-    for idx, header in enumerate(headers, start=1):
-        ws.cell(row=1, column=idx, value=header)
+for sheet in sheets: # Itera sobre as abas
+    ws = wb.create_sheet(f'{sheet}') # Cria cada aba inicia o preenchimento no proximo laço
+    for idx, header in enumerate(headers, start=1): # Itera sobre a aba atual e cria os cabeçalhos
+        ws.cell(row=1, column=idx, value=header) # Escreve os cabeçalhos
 
-    if sheet != 'Resumo':
+    if sheet != 'Resumo': # Verifica se a aba do laço é 'Resumo', pois não iremos preencher as linhas e colunas dela
         produtos = ['Uva', 'Morango', 'Maçã', 'Manga', 'Kiwi']
 
-        for row in range(2, 2 + len(produtos)):
+        for row in range(2, 2 + len(produtos)): # Itera sobre cada linha, iniciando pela linha 2 e terminando em len(produtos), ou seja, da linha 2 à linha 6
+            # como len(produtos) = 5, soma-se 2 a ela pois estamos iniciando na linha 2 e porque o range não captura o ultimo valor, então para que a linha 6
+            # seja capturada, precisamos iterar da linha 2, 2 + 5 = 7
             nome_produto = cria_nome_produto(produtos)
             produtos.remove(nome_produto)
             quantidade_produto = random.randint(1, 20)
--- frutas/ex11.py
+++ frutas/ex11.py
@@ -1,23 +1,27 @@
 from openpyxl import load_workbook
 
 wb = load_workbook('dados.xlsx')
-ws = wb['Janeiro']
 
 col_produto = 1 # Coluna A
 col_quantidade = 2 # Coluna B
 col_preco_unitario = 3 # Coluna C
 col_total = 4 # Coluna D
 
-for row in ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=1, max_col=4):
-    linha = row[0].row # Como o iter_rows me dá uma tupla com todas as colunas da linha, utilizei o row[0] para pegar a celula da coluna quantidade
-    # e o row[0].row para pegar o numero exato da celula
+for sheet in wb.sheetnames: # Iterando sobre as abas da planilha 'dados'
+    if sheet == 'Vendas' and sheet == 'Relatorio': # Evitando que acesse as planilhas de vendas e relatorio
+        continue
+    ws = wb[sheet] # Ativando a planilha da iteração atual do laço
 
-    cell_valor_quantidade = ws.cell(row=linha, column=col_quantidade)
-    cell_valor_preco = ws.cell(row=linha, column=col_preco_unitario)
-    cell_total = ws.cell(row=linha, column=col_total)
+    for row in ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=1, max_col=4): # Iterando sobre todas as linhas até a ultima coluna
+        linha = row[0].row # Como o iter_rows me dá uma tupla com todas as colunas da linha, utilizei o row[0] para pegar a celula da coluna quantidade
+        # e o row[0].row para pegar o numero exato da celula
 
-    formula_total = f'={cell_valor_quantidade.coordinate}*{cell_valor_preco.coordinate}' # =B1*C1
-    cell_total.value = formula_total
+        cell_valor_quantidade = ws.cell(row=linha, column=col_quantidade) # Armazenando a célula de quantidade de cada linha
+        cell_valor_preco = ws.cell(row=linha, column=col_preco_unitario) # Armazenando a célula de preço de cada linha
+        cell_total = ws.cell(row=linha, column=col_total) # Armazenando a célula de total de cada linha
+
+        formula_total = f'={cell_valor_quantidade.coordinate}*{cell_valor_preco.coordinate}' # =B1*C1
+        cell_total.value = formula_total # Pegando o valor total do calculo de cada linha
 
 wb.save('dados.xlsx')
 
